e88d7fd187d78839d8132f057258b744c1ec35cc
Kurt Weiberth <kurt.weiberth@gmail.com>
2016-02-05 09:28:46 -0800

    Initial set up of bare repository ready for part 3

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('./dist'));
+
+app.use('/', function (req, res) {
+    res.sendFile(path.resolve('client/index.html'));
+});
+
+var port = 3000;
+
+app.listen(port, function(error) {
+  if (error) throw error;
+  console.log("Express server listening on port", port);
+});

6e632d269af730bafe7e671136547bcb39f68a16
Kurt Weiberth <kurt.weiberth@gmail.com>
2016-02-06 13:28:43 -0800

    Complete part 6, ready for part 8

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react'
 
 class TextDisplay extends Component {
 
+  handleClick() {
+    this.props.deleteLetter()
+  }
+
   render() {
-    return <div>I'm displaying text from my parent: {this.props.text}</div>
+    return (
+      <div>
+        <div>I'm displaying text from my parent: {this.props.text}</div>
+        <button onClick={this.handleClick.bind(this)}>delete one letter</button>
+      </div>
+    )
   }
 
 }
--- components/TextInput.js
+++ components/TextInput.js
@@ -10,6 +10,12 @@ class TextInput extends Component {
     }
   }
 
+  deleteLetter() {
+    this.setState({
+      inputText: this.state.inputText.substring(0, this.state.inputText.length - 1)
+    })
+  }
+
   handleChange(event) {
     this.setState({
       inputText: event.target.value
@@ -25,7 +31,7 @@ class TextInput extends Component {
           value={this.state.inputText}
           onChange={this.handleChange.bind(this)}
         />
-        <TextDisplay text={this.state.inputText}/>
+        <TextDisplay text={this.state.inputText} deleteLetter={this.deleteLetter.bind(this)}/>
       </div>
     )
   }

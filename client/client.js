c80b1a6162d1d9d792bac749ef12dc3bf628731f
Taki Vlasakakis <takivlasakakis@TVlasakakis-MacBook-Pro.local>
2017-02-07 10:09:49 -0700

    react tutorial part 3, at 21:43, added code to client.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
 
+render(
+	// define the encompassing component,
+	// DOM element we want to mount it to
+	<App/>,
+	document.getElementById('app')
+
+	)

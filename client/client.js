cd970024add77f8028305f637b7512cbed3dfc32
Taki Vlasakakis <takivlasakakis@TVlasakakis-MacBook-Pro.local>
2017-02-07 12:50:09 -0700

    finish part 3 of tutorial

--- client/client.js
+++ client/client.js
@@ -9,3 +9,5 @@ render(
 	document.getElementById('app')
 
 	)
+
+
--- components/App.js
+++ components/App.js
@@ -1,1 +1,10 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+
+	render () {
+		return <div> This is definitely a React app now!</div>
+	}
+}
+
+export default App

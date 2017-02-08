b9eebbcbd1974298d517bf6e1c6d87e3bf8337be
Taki Vlasakakis <takivlasakakis@TVlasakakis-MacBook-Pro.local>
2017-02-08 12:15:07 -0700

    start part 6, add delete letter functionality.

--- components/TextInput.js
+++ components/TextInput.js
@@ -10,6 +10,15 @@ class TextInput extends Component {
     }
   }
 
+  deleteLetter() {
+  	// take current this.state.inputText
+  	// delete letter
+  	// update state
+  	this.setState({
+  		inputText: this.state.inputText.substring(0, this.state.inputText.length - 1)
+  	});
+  }
+
   handleChange(event) {
     this.setState({
       inputText: event.target.value
@@ -25,7 +34,7 @@ class TextInput extends Component {
           value={this.state.inputText}
           onChange={this.handleChange.bind(this)}
         />
-        <TextDisplay text={this.state.inputText}/>
+        <TextDisplay text={this.state.inputText} deleteLetter={this.deleteLetter.bind(this)}/>
       </div>
     )
   }
